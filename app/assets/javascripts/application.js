9e1ab3f623dafcf7fc930a04b0e66b803724a80d
Sumit Gupta <sumitngupta@gmail.com>
2013-02-27 20:41:45 +0800

    darkening the spinner so it doesnt stick at much. the texture makes it less necessary to call attention to it.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,7 +29,7 @@ $(function () {
     width = $(window).width(),
     url = 'http://i.embed.ly/1/image/resize?width=' + headerWidth(width) + '&url=http%3A%2F%2Fblog.sumitngupta.com%2Fassets%2Fcovers%2F2-bca392396952ed0909f5c37d38bfddf5.jpg&key=fb3c4ef788e74e888046dabf72245ec0/';
 
-  spinner = new Spinner({color: '#666'}).spin();
+  spinner = new Spinner({color: '#333'}).spin();
   $spinner.append(spinner.el);
 
   $.imgpreload(url, function () {
