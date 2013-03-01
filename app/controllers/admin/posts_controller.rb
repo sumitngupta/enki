64afc24eed6d1c287f23826270ed67260fefd347
Sumit Gupta <sumitngupta@gmail.com>
2013-03-01 16:22:04 +0800

    fix preview

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -60,7 +60,7 @@ class Admin::PostsController < Admin::BaseController
 
     respond_to do |format|
       format.js {
-        render :partial => 'posts/post', :locals => {:post => @post}
+        render :partial => 'posts/post', :locals => {:post => @post, :mini => false}
       }
     end
   end
