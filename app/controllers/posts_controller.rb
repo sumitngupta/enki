089300cfd73eb5432449af9daca6055bb1a80e91
Sumit Gupta <sumitngupta@gmail.com>
2013-03-02 22:13:27 +0800

    annoying post not found error squashing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
   def index
     @tag = params[:tag]
     @posts = Post.find_recent(:tag => @tag, :include => :tags)
@@ -11,5 +13,10 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {:include => [:tags]}))
+  end
+
+  def record_not_found
+    flash[:notice] = "That post was not found"
+    redirect_to root_path
   end
 end
